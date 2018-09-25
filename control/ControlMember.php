f9e5302d9b0d0c8d107458229ace4a01bc383314
黃琮壬 <shrekhuang2186@gmail.com>
2018-09-25 14:12:17 +0800

    new login function in controlMember

--- control/ControlMember.php
+++ control/ControlMember.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace shrek;
 include_once "database/ConnectDB.php";
+
 use shrek\ConnectDB as CDB;
 
 class ControlMember
@@ -8,11 +10,12 @@ class ControlMember
     public $video;
     public $playlist;
     public $connect;
+
     function __construct()
     {
         $cdb = new CDB();
         $cdb->server_name = "localhost";
-        $cdb->database_name = "Member";
+        $cdb->database_name = "member";
         $cdb->user_name = "root";
         $cdb->password = "";
         $this->connect = $cdb->Connection();
@@ -20,10 +23,31 @@ class ControlMember
 
     function SelectPlayList($videoID)
     {
-        $select = $this->connect->prepare("SELECT * FROM playlist WHERE ID=:id ");
+        $select = $this->connect->prepare("SELECT * FROM playlist WHERE ID LIKE :id ");
         $select->bindValue(':id', $videoID, \PDO::PARAM_STR);
         $select->execute();
         $result = $select->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    function Login($email, $password)
+    {
+        $login_message = '';
+        try {
+            $select = $this->connect->prepare("SELECT email,password FROM information WHERE email LIKE :em");
+            $select->bindValue(':em', $email, \PDO::PARAM_STR);
+            if ($select->execute()) {
+                $result = $select->fetchAll(\PDO::FETCH_ASSOC);
+                if (count($result)) {
+                    if ($result['password'] == $password) {
+                        setcookie('verification', $result['email'], time() + 3600, '/');
+                        $login_message = 'login success';
+                    } else $login_message = 'password error';
+                } else $login_message = 'email error';
+            } else $login_message = 'server error';
+        } catch (\PDOException $e) {
+            echo "Select information failed: " . $e->getMessage();
+        }
+        return $login_message;
     }
 
 }
