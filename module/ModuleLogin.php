317914dca46d297ed87ee6c57deb20985f84df39
黃琮壬 <shrekhuang2186@gmail.com>
2018-09-25 14:18:57 +0800

    new moduleLogin

---
+++ module/ModuleLogin.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once "../control/ControlMember.php";
+use shrek\ControlMember as CM;
+
+$cm = new CM();
+$email= $_POST['email'];
+$password = $_POST['password'];
+$login_message = $cm->Login($email,$password);
+echo $login_message;
