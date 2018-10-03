d429954edf2271af0481ab0ce504267a4378c7b6
黃琮壬 <shrekhuang2186@gmail.com>
2018-10-03 14:34:40 +0800

    登入顯示

--- content_member.php
+++ content_member.php
@@ -36,7 +36,53 @@ $r['Video_Watch_Num']='1280';?>
                                         </div>
                                         <!--    個人介紹   -->
                                         <div class="col-lg-10">
+                                            <?php if (isset($_COOKIE['memberID'])) { ?>
                                             <div id="chartContainer" style="height: 300px; width: 100%;"></div>
+                                            <?php }else{?>
+                                                <h3 style="margin-bottom: 20px">我最近的上傳</h3>
+                                                <div >
+                                                    <form id="<?php echo $r['Video_ID'] ?>" method="post"
+                                                          action="video_play.php?v=<?php echo $r['Video_ID'] ?>">
+                                                        <input type="hidden" id="Query" name="Query"
+                                                               value="<?php echo $_GET["Search_Text"]; ?>"/>
+                                                    </form>
+                                                    <div class="row"
+                                                         onclick="document.getElementById('<?php echo $r["Video_ID"] ?>').submit();"
+                                                         style="cursor: pointer">
+                                                        <div class="col-md-4">
+                                                            <!--                                                            <img style="border-radius: 0.3em;" class="img-responsive"-->
+                                                            <!--                                                                 src="source/image/video_img/--><?php //echo $r['Video_ID'] ?><!--.png"-->
+                                                            <!--                                                                 alt="Norway">-->
+                                                            <img style="width: 100%;border-radius: 0.3em;" src="libs/img/justin-timberlake-Say%20Something.jpg">
+                                                        </div>
+
+                                                        <div class="col-md-8" style="    font-size: 18px;
+    letter-spacing: 1px; ">
+                                                            <div class="row">
+                                                                <div class="col-sm-12">
+                                                                    <div><?php echo $r['Video_Name'] ?></div>
+                                                                </div>
+                                                                <div class="col-sm-12"
+                                                                     style="position: relative;height: 150px;float: right">
+                                                                    <div style="position: absolute;top: 0;left: 0;height: 100px;width: 100%;">
+                                                                        <div class="row">
+                                                                            <div class="col-sm-12" style="height: 50px;line-height: 50px">
+                                                                                <i style="font-size: 22px" class="fa fa-user-circle-o"></i><?php echo $r['Video_Uploader'] ?>
+                                                                                <i style="font-size: 22px" class="fa fa-clock-o"></i><?php echo $r['Video_Date'] ?>
+                                                                                <i style="font-size: 25px" class="fa fa-play-circle-o"></i><?php echo $r['Video_Watch_Num'] ?>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div style=" font-size: 20px;font-weight: 600;position: absolute;bottom: 0;right: 1em">
+                                                                        $ 490 NT
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            <?php }?>
+
                                         </div>
                                     </div>
                                 </div>
@@ -57,9 +103,11 @@ $r['Video_Watch_Num']='1280';?>
                     <div class="col-sm-12 title-name subtitle">
                         <ul class="nav navbar-nav">
                             <li class="active"><a data-toggle="tab" href="#menu-1">影片清單</a></li>
-                            <li><a data-toggle="tab" href="#menu-2">代言邀請</a></li>
-                            <li><a data-toggle="tab" href="#menu-3">通知</a></li>
-                            <li><a data-toggle="tab" href="#menu-4">社群</a></li>
+                            <?php if (isset($_COOKIE['memberID'])) { ?>
+                                <li><a data-toggle="tab" href="#menu-2">代言邀請</a></li>
+                                <li><a data-toggle="tab" href="#menu-3">通知</a></li>
+                                <li><a data-toggle="tab" href="#menu-4">社群</a></li>
+                            <?php }?>
                         </ul>
                     </div>
                 </div>
@@ -78,12 +126,16 @@ $r['Video_Watch_Num']='1280';?>
                             <div id="menu-1" class="subtitle tab-pane fade in active">
                                 <div class="title-name subtitle">
                                     <ul class="nav navbar-nav">
-                                        <li class="active"><a data-toggle="tab" href="#submenu-0">上傳</a></li>
-                                        <li><a data-toggle="tab" href="#submenu-1">頻道</a></li>
-                                        <li><a data-toggle="tab" href="#submenu-2">收藏</a></li>
+
+                                        <li class="active"><a data-toggle="tab" href="#submenu-1">頻道</a></li>
+                                        <?php if (isset($_COOKIE['memberID'])) { ?>
+                                            <li class="active"><a data-toggle="tab" href="#submenu-0">上傳</a></li>
+                                            <li><a data-toggle="tab" href="#submenu-1">頻道</a></li>
+                                            <li><a data-toggle="tab" href="#submenu-2">收藏</a></li>
+                                        <?php }?>
                                     </ul>
                                 </div>
-                                <div class="tab-content">
+                                <div class="tab-content"><?php if (isset($_COOKIE['memberID'])) { ?>
                                     <div id="submenu-0" class="subtitle tab-pane fade in active">
                                         <div class="float-box">
                                             <h3 style="margin-bottom: 20px">我最近的上傳</h3>
@@ -95,6 +147,7 @@ $r['Video_Watch_Num']='1280';?>
 <!--                                                <p>Justin Timberlake - Say Something (Official Video) ft. Chris Stapleton</p>-->
 <!--                                            </div>-->
 <!--                                        </div>-->
+
                                             <div >
                                                 <form id="<?php echo $r['Video_ID'] ?>" method="post"
                                                       action="video_play.php?v=<?php echo $r['Video_ID'] ?>">
@@ -136,14 +189,17 @@ $r['Video_Watch_Num']='1280';?>
                                                     </div>
                                                 </div>
                                             </div>
+
                                         </div>
                                     </div>
+                                        <div id="submenu-2" class="subtitle tab-pane fade">
+                                            <div class="float-box">我收藏的影片</div>
+                                        </div>
+                                    <?php }?>
                                     <div id="submenu-1" class="subtitle tab-pane fade ">
                                         <div class="float-box">我建立的頻道</div>
                                     </div>
-                                    <div id="submenu-2" class="subtitle tab-pane fade">
-                                        <div class="float-box">我收藏的影片</div>
-                                    </div>
+
                                 </div>
 
                             </div>
